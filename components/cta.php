ab48522cafacd1dde08b5539a350df051a0af83d
satyam mishra <satyamjay007@gmail.com>
2024-08-13 12:36:54 +0530

    css and images udpates

--- components/cta.php
+++ components/cta.php
@@ -2,6 +2,8 @@
     <div class="cta-overlay"></div>
     <div class="cta-content">
         <h1>Don't delay <span>Care for you</span> and those you love</h1>
-        <a class="main-button" href="https://saaol.com/contact-saaol">Contact now</a>
+        <button class="main-button">
+            <a class="" href="https://saaol.com/contact-saaol">Contact now</a>
+        </button>
     </div>
 </section>
